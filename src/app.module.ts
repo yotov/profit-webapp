d17fc45ae4727c07e7e7181a16a3aa158dcb8570
Ilian Yotov <ilian@yotov.eu>
2023-10-16 23:10:46 +0300

    feat: Add base script to generate sample data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfitService } from './profit.service';
+import { PriceHistoryCommandModule } from './price-history-command/price-history-command.module';
 
 @Module({
-  imports: [],
+  imports: [PriceHistoryCommandModule],
   controllers: [AppController],
   providers: [AppService, ProfitService],
 })
---
+++ src/price-history-command/price-history-command.module.ts
@@ -0,0 +1,68 @@
+import { Command, CommandRunner, Option } from 'nest-commander';
+import { writeFile } from 'node:fs/promises';
+
+interface CommandOptions {
+  startTime?: Date;
+  startPrice?: number;
+  count?: number;
+}
+
+function round(value: number, precision = 2): number {
+  const res = Math.pow(10, precision);
+  return Math.round(value * res) / res;
+}
+
+function getRandomPercentageChange(): number {
+  const rand = Math.random();
+  if (rand < 0.3) {
+    return 1;
+  } else if (rand < 0.8) {
+    return 1 + rand / 1000;
+  }
+  return 1 - rand / 1000;
+}
+
+@Command({
+  name: 'generate-price-history',
+  description: 'A cli script that generates sample data',
+})
+export class PriceHistoryCommandModule extends CommandRunner {
+  async run(passedParam: string[], options?: CommandOptions): Promise<void> {
+    const startTime = (options?.startTime ?? new Date()).setMilliseconds(0);
+    const count = options?.count ?? 10_000;
+    let price = options?.startPrice ?? 100;
+
+    const content = [...Array(count).keys()]
+      .map((i) => {
+        price = round(price * getRandomPercentageChange(), 3);
+        return `${new Date(startTime + i * 1000).toISOString()},${price}`;
+      })
+      .join('\n');
+
+    await writeFile('./data/history.csv', content);
+  }
+
+  @Option({
+    flags: '-c, --count [number]',
+    description: 'The count of prices in sample data',
+  })
+  parseCount(val: string): number {
+    return Number(val);
+  }
+
+  @Option({
+    flags: '-s, --start-price [number]',
+    description: 'The starting price of sample data',
+  })
+  parseStartPrice(val: string): number {
+    return Number(val);
+  }
+
+  @Option({
+    flags: '-d, --start-date [date]',
+    description: 'The starting date of sample data ( in "yyyy-MM-dd" format )',
+  })
+  parseStartDate(val: string): Date {
+    return new Date(val); // @TODO: use date-fns
+  }
+}
