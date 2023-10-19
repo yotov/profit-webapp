50787ffafdc5a8b2a408eaa151768c50114b7df0
Ilian Yotov <ilian@yotov.eu>
2023-10-19 11:39:15 +0300

    refactor: Style form level error, add ErrorBoundery and improve type for form

--- workspaces/ui/src/App.tsx
+++ workspaces/ui/src/App.tsx
@@ -1,88 +1,19 @@
-import { useState } from 'react'
 import './App.css'
-import DateTimePicker from 'react-datetime-picker';
 import 'react-datetime-picker/dist/DateTimePicker.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
-import Loader from './components/Loader';
-
-type RequestState =
-  | { status: 'idle' }
-  | { status: 'loading' }
-  | { status: 'success', data: any }
-  | { status: 'error', error: any };
-
-type FormState = {
-  startTime: Date,
-  endTime: Date
-}
-
-function clean(obj: any) {
-  for (const propName in obj) {
-    if (obj[propName] === null || obj[propName] === undefined) {
-      delete obj[propName];
-    }
-  }
-  return obj
-}
+import { ErrorBoundary } from "react-error-boundary";
+import Form from './components/Form';
 
 function App() {
-  const [requestState, setRequestState] = useState<RequestState>({ status: 'idle' });
-  const [formState, setFormState] = useState<FormState>({ startTime: new Date(), endTime: new Date() });
 
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
-    if (requestState.status === "loading") {
-      return;
-    }
-    setRequestState({ status: 'loading' });
-
-    try {
-      const params = new URLSearchParams(clean(formState));
-      const response = await fetch(`/api/profit?${params}`, {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-        },
-      });
-      // Simulate slow request to prevent blink of loader 
-      await new Promise(resolve => setTimeout(resolve, 350));
-
-      const result = await response.json();
-      if (response.ok) {
-        setRequestState({ status: 'success', data: result });
-      } else {
-        setRequestState({ status: 'error', error: result });
-      }
-
-    } catch (err) {
-      setRequestState({ status: 'error', error: "Please try again later." });
-    }
-  };
 
   return (
     <>
       <h1>Profit</h1>
-      <form className={"form " + (requestState.status === "loading" ? "form-loading" : "")} onSubmit={handleSubmit}>
-        <label className={'field ' + (requestState.status == "error" && requestState.error.fields.startTime ? 'field-has-error' : '')}>
-          <div className='field-label'>Start time:</div>
-          <DateTimePicker autoFocus onChange={newValue => setFormState({ startTime: newValue, endTime: formState.endTime })} format="y-MM-dd HH:mm:ss" value={formState.startTime} />
-          {requestState.status == "error" && requestState.error.fields.startTime && <div className="field-error">{requestState.error.fields.startTime}</div>}
-        </label>
-        <label className={'field ' + (requestState.status == "error" && requestState.error.fields.endTime ? 'field-has-error' : '')}>
-          <div className='field-label'>End time:</div>
-          <DateTimePicker onChange={newValue => setFormState({ startTime: formState.startTime, endTime: newValue })} format="y-MM-dd HH:mm:ss" value={formState.endTime} />
-          {requestState.status == "error" && requestState.error.fields.endTime && <div className="field-error">{requestState.error.fields.endTime}</div>}
-        </label>
-        <div>
-          <button type="submit">
-            Find max profit
-            {requestState.status === "loading" && <Loader />}
-          </button>
-        </div>
-        {requestState.status == "success" && <div>You will realize max profit if you buy at {requestState.data.buyTime} and sell at {requestState.data.sellTime}. The profit will be {requestState.data.profit}</div>}
-        {requestState.status == "error" && !requestState.error.fields && <div className="form-error">{requestState.error.message}</div>}
-      </form>
+      <ErrorBoundary fallback={<p>⚠️Something went wrong</p>}>
+        <Form />
+      </ErrorBoundary>
     </>
   )
 }
---
+++ workspaces/ui/src/components/Form.tsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import DateTimePicker from 'react-datetime-picker';
+import Loader from '../components/Loader';
+
+type Error = {
+    message: string,
+    fields?: Record<string, string>,
+}
+
+type RequestState =
+    | { status: 'idle' }
+    | { status: 'loading' }
+    | { status: 'success', data: any }
+    | { status: 'error', error: Error };
+
+type FormState = {
+    startTime: string | undefined,
+    endTime: string | undefined
+}
+
+function clean(obj: any) {
+    for (const propName in obj) {
+        if (obj[propName] === null || obj[propName] === undefined) {
+            delete obj[propName];
+        }
+    }
+    return obj
+}
+
+
+const Form = () => {
+    const [requestState, setRequestState] = useState<RequestState>({ status: 'idle' });
+    const [formState, setFormState] = useState<FormState>({ startTime: new Date(2023, 9, 19, 10, 58, 3).toISOString(), endTime: new Date(2023, 9, 19, 10, 58, 4).toISOString() });
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        if (requestState.status === "loading") {
+            return;
+        }
+        setRequestState({ status: 'loading' });
+
+        try {
+            const params = new URLSearchParams(clean(formState));
+            const response = await fetch(`/api/profit?${params}`, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                },
+            });
+            // Simulate slow request to prevent blink of loader 
+            await new Promise(resolve => setTimeout(resolve, 350));
+
+            const result = await response.json();
+            if (response.ok) {
+                setRequestState({ status: 'success', data: result });
+            } else {
+                setRequestState({ status: 'error', error: result });
+            }
+
+        } catch (err) {
+            setRequestState({ status: 'error', error: { message: "Please try again later." } });
+        }
+    };
+
+    return <form className={"form " + (requestState.status === "loading" ? "form-loading" : "") + (requestState.status == "error" && !requestState.error.fields ? "form-has-error" : '')} onSubmit={handleSubmit}>
+        {requestState.status == "error" && !requestState.error.fields && <div className="form-error">{requestState.error.message}</div>}
+        <label className={'field ' + (requestState.status == "error" && requestState.error.fields?.startTime ? 'field-has-error' : '')}>
+            <div className='field-label'>Start time:</div>
+            <DateTimePicker autoFocus onChange={newValue => setFormState({ startTime: newValue?.toISOString(), endTime: formState.endTime })} format="y-MM-dd HH:mm:ss" value={formState.startTime} />
+            {requestState.status == "error" && requestState.error.fields?.startTime && <div className="field-error">{requestState.error.fields.startTime}</div>}
+        </label>
+        <label className={'field ' + (requestState.status == "error" && requestState.error.fields?.endTime ? 'field-has-error' : '')}>
+            <div className='field-label'>End time:</div>
+            <DateTimePicker onChange={newValue => setFormState({ startTime: formState.startTime, endTime: newValue?.toISOString() })} format="y-MM-dd HH:mm:ss" value={formState.endTime} />
+            {requestState.status == "error" && requestState.error.fields?.endTime && <div className="field-error">{requestState.error.fields.endTime}</div>}
+        </label>
+        <div>
+            <button type="submit">
+                Find max profit
+                {requestState.status === "loading" && <Loader />}
+            </button>
+        </div>
+        {requestState.status == "success" && <div>You will realize max profit if you buy at {requestState.data.buyTime} and sell at {requestState.data.sellTime}. The profit will be {requestState.data.profit}</div>}
+    </form>
+}
+
+export default Form;
